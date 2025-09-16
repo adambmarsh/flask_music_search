a669a12e87ea52c5e182b65658047407d1ee6430
oskar <oskarbukolt@gmail.com>
2025-09-16 11:49:15 +0100

    Save volume from previous session using cookies (commit corrected).

--- static/player.js
+++ static/player.js
@@ -2,9 +2,11 @@ export class Player {
     constructor(){
     this.player = document.getElementById('player');
     this.player.addEventListener("ended",this.handleEvents);
+    this.player.addEventListener("volumechange",this.handleEvents);
 
     this.src = ""
     this.trackNo = 0;
+    this.srcSet = false;
     }
     async play(){
         try{
@@ -18,6 +20,10 @@ export class Player {
     set src(value) {
         this._src = value;
         this.player.src = this._src;
+        if(this.srcSet === false){
+            this.player.volume = this.getVolumeFromCookie();
+            this.srcSet = true;
+        }
         return;
     }
     get src() {
@@ -27,6 +33,9 @@ export class Player {
         switch(event.type){
             case "ended":
                 this.playNextTrack();
+                break;
+            case "volumechange":
+                this.updateVolumeCookie();
                 break;
             default:
                 console.log("Unregistered event.type detected.")
@@ -52,5 +61,30 @@ export class Player {
             this.src = uri;
         }
         this.play();
+    }
+    updateVolumeCookie(){
+        let d = new Date();
+        const month = d.getMonth();
+        if (month+3 < 13){
+            d.setMonth(month+3);
+        }
+        else{
+            let year = d.getYear();
+            d.setYear(year+1);
+            d.setMonth((month+3 % 12));
+        }
+        const formatted_date = d.toUTCString();
+        const path = "path=/";
+        let volume = this.player.volume;
+        document.cookie = `volume=${volume};expires=${formatted_date};${path}`;
+    }
+    getVolumeFromCookie(){
+        let volume = this.player.volume;
+        if(document.cookie != ''){
+            let cookieArray = document.cookie.split('=');
+            volume = parseFloat(cookieArray[1]);
+        }
+        return volume;
+
     }
 }
